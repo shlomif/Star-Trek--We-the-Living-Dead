b0bca79e173016b14bb0b642daf62ea28ec49927
Shlomi Fish <shlomif@shlomifish.org>
2024-10-16 07:45:00 +0300

    Fix tidyall tests.
    See:
    
    https://metacpan.org/release/Code-TidyAll .

--- star-trek--we-the-living-dead/screenplay/scripts/prepare-epub.pl
+++ star-trek--we-the-living-dead/screenplay/scripts/prepare-epub.pl
@@ -5,13 +5,12 @@ use warnings;
 
 use utf8;
 
-use Shlomif::Screenplays::EPUB;
+use Shlomif::Screenplays::EPUB ();
 
 my $gfx = 'Star-Trek--We-the-Living-Dead-cover.svg.png';
 my $obj = Shlomif::Screenplays::EPUB->new(
     {
-        images =>
-        {
+        images => {
             $gfx => "images/$gfx",
         },
     }
@@ -25,43 +24,37 @@ $obj->run;
 
     $obj->output_json(
         {
-            data =>
-            {
+            data => {
                 filename => $epub_basename,
-                title => q/Star Trek: “We, the Living Dead”/,
-                authors =>
-                [
+                title    => q/Star Trek: “We, the Living Dead”/,
+                authors  => [
                     {
                         name => "Shlomi Fish",
                         sort => "Fish, Shlomi",
                     },
                 ],
-                contributors =>
-                [
+                contributors => [
                     {
                         name => "Shlomi Fish",
                         role => "oth",
                     },
                 ],
-                cover => "images/$gfx",
-                rights => "Creative Commons Attribution ShareAlike Unported 3.0 (CC-by-sa-3.0)",
+                cover  => "images/$gfx",
+                rights =>
+"Creative Commons Attribution ShareAlike Unported 3.0 (CC-by-sa-3.0)",
                 publisher => 'http://www.shlomifish.org/',
-                language => 'en-GB',
-                subjects => [
-                    "Buffy",
-                    "Deep Space Nine",
-                    "DS9",
-                    "FICTION/Humorous",
-                    "FICTION/Science Fiction/General",
-                    "History",
-                    "Near East History",
-                    "Judaism",
+                language  => 'en-GB',
+                subjects  => [
+                    "Buffy",                           "Deep Space Nine",
+                    "DS9",                             "FICTION/Humorous",
+                    "FICTION/Science Fiction/General", "History",
+                    "Near East History",               "Judaism",
                     "Star Trek",
                 ],
-                identifier =>
-                {
+                identifier => {
                     scheme => 'URL',
-                    value => 'http://www.shlomifish.org/humour/Star-Trek/We-the-Living-Dead/',
+                    value  =>
+'http://www.shlomifish.org/humour/Star-Trek/We-the-Living-Dead/',
                 },
             },
         },
